87d3615e1f18519d8fee17487ba9a0379d1f9442
NazmulHasanNahid <nazmulhasan3394@gmail.com>
2022-03-26 11:29:43 +0600

    modal

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,13 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import {MdRemoveShoppingCart} from 'react-icons/md'
 import {FaCommentMedical} from 'react-icons/fa'
+import Modal from 'react-modal';
 import Cart from '../Cart/Cart';
 import  './Books.css'
 import ShowBooks from './ShowBooks';
 
+
+
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+  Modal.setAppElement('#root');
+
 const Books = () => {
     const [books , setBooks] = useState([])
     const [cart , setCart] = useState([])
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+      }
+    
+      
+      function closeModal() {
+        setIsOpen(false);
+      }
+
+
+
     useEffect(() =>{
         fetch('books.json')
          .then(res => res.json())
@@ -20,6 +48,11 @@ const Books = () => {
     const chooseBtn = () =>{
         setCart([])
     }
+
+
+
+    
+    
     return (
         <div className='books-container container'>
             <div class="row row-cols-1 row-cols-md-3 g-4 container mx-auto my-3">
@@ -36,11 +69,26 @@ const Books = () => {
             <h4 className='fw-bold text-info  '>Selecd Items : {cart.length} </h4>
                {
                    cart.map(item => <Cart key={item.id} item={item}></Cart>)
+                   
                }
                <div className='btn-custom'>
-               <button className='btn btn-info text-white m-2'>Choose 1 For Me <FaCommentMedical size={25}/></button>
+               <button onClick={openModal}  className='btn btn-info text-white m-2'>Choose 1 For Me <FaCommentMedical size={25}/></button>
                <button onClick={chooseBtn} className='btn btn-info text-white m-2 custom-margin  d-flex align-items-center '>Choose Again <MdRemoveShoppingCart size={30} /> </button>
                </div>
+
+               <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+                >
+                   
+                    {
+                       cart.map(item => <Cart key={item.id} item={item}></Cart>)
+                   
+                     }
+                    <button onClick={closeModal} className='btn btn-info text-white'>Close</button>
+                </Modal>
             </div>
             
         </div>
--- src/components/Books/ShowBooks.js
+++ src/components/Books/ShowBooks.js
@@ -4,28 +4,26 @@ import { BsFillCartCheckFill } from 'react-icons/bs';
 import './Books.css'
 
 const ShowBooks = ({book , handleBtn}) => {
+ 
     const {name , img , price} = book
    
     return (
-      <div className='showbooks-container'>
-        <div class="col"> 
+      <div className="showbooks-container">
+        <div class="col">
           <div class="card h-75 rounded-lg">
             <img src={img} class="card-img-top" alt="..." />
             <div class="card-body">
-              <h5 class="card-title text-danger fw-bold">
-                {name}
-              </h5>
-              <p class="card-text">
-               price : ${price}
-              </p>
-              <p class="card-text">
-              
-              </p>
+              <h5 class="card-title text-danger fw-bold">{name}</h5>
+              <p class="card-text">price : ${price}</p>
+              <p class="card-text"></p>
             </div>
             <div class="card-footer border-0 mx-auto">
-              <button onClick={()=>handleBtn(book)} className='btn btn-info d-flex align-items-center     text-white fw-bold'>
-                  Add To Cart 
-                  <BsFillCartCheckFill size={25}/>
+              <button
+                onClick={() => handleBtn(book)}
+                className="btn btn-info d-flex align-items-center  text-white fw-bold"
+              >
+                Add To Cart
+                <BsFillCartCheckFill size={25} />
               </button>
             </div>
           </div>
